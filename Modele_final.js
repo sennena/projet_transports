eecea628ec9e7f34fa717851e4528a1455157284
etiennegeorges <78967202+etiennegeorges@users.noreply.github.com>
2021-03-12 16:21:51 +0100

    Update Modele_final.js

--- Modele_final.js
+++ Modele_final.js
@@ -416,7 +416,7 @@ function simulation(telecharg){
             if(valid){liste_lignes.push(i);}
         }
     
-        var min_li= Math.min(Nb_max_rames,liste_lignes.length,10);
+        var min_li= Math.min(Nb_max_rames,liste_lignes.length,50);
         var matrice_trains_graph= new Matrice(min_li,Tot_blocs);
         
         for (var ind=0; ind<liste_lignes.length;ind++){
@@ -892,8 +892,57 @@ function simulation(telecharg){
     function animation_graphique(Matrice,ctx) {
         var Troncons = [200, 210, 210, 210, 210, 210, 233, 233, 233, 235, 235, 235, 235, 207, 207, 207, 207, 226, 226, 226, 205, 205, 230, 230, 213, 213, 213, 215, 215, 233, 233, 233, 295, 295, 225, 225, 210, 210, 270, 270, 350, 218, 218, 218, 243, 243, 243, 215, 215, 215, 215, 247, 247, 247, 247, 223, 223, 223, 222, 222, 222, 222, 200, 200, 200, 200, 263, 263, 263, 210, 210, 210, 210, 200, 200, 210, 210, 210, 210, 263, 263, 263, 200, 200, 200, 200, 222, 222, 222, 222, 223, 223, 223, 247, 247, 247, 247, 215, 215, 215, 215, 243, 243, 243, 218, 218, 218, 350, 270, 270, 210, 210, 225, 225, 295, 295, 233, 233, 233, 215, 215, 213, 213, 213, 230, 230, 205, 205, 226, 226, 226, 207, 207, 207, 207, 235, 235, 235, 235, 233, 233, 233, 210, 210, 210, 210, 210, 200];
         var Station = [-1, 0, -1, -1, -1, -1, 1, -1, -1, 2, -1, -1, -1, 3, -1, -1, -1, 4, -1, -1, 5, -1, 6, -1, 7, -1, -1, 8, -1, 9, -1, -1, 10, -1, 11, -1, 12, -1, 13, -1, 14, 15, -1, -1, 16, -1, -1, 17, -1, -1, -1, 18, -1, -1, -1, 19, -1, -1, 20, -1, -1, -1, 21, -1, -1, -1, 22, -1, -1, 23, -1, -1, -1, 24];
-        var Nom_stations = ["La_Defense", "Esplanade_de_la_Defense", "Pont_de_Neuilly", "Les_Sablons", "Porte_Maillot", "Argentine", "Charles_de_Gaulle-Etoile", "George_V", "FDRoosevelt", "Champs-Elysees_Clémenceau", "Concorde", "Tuileries", "Palais_Royal-Musee_du_Louvre", "Louvre_Rivoli", "Chatelet", "Hotel_de_Ville", "Saint_Paul", "Bastille", "Gare_de_Lyon", "Reuilly-Diderot", "Nation", "Porte_de_Vincennes", "Saint_Mande-Tourelle", "Berault", "Chateau_de_Vincennes"];
+        var avec_corres = [true, false, false, false, true, false, true, false, true, true, true, false, true, false, true, true, false, true, true, true, true, true, false, false, false];
+
+        // Dessin de la ligne
+        function dessine_Ligne_1() {
+            var l;
+            var c;
+            l = 0;
+            for (var i=0; i<74; i++) {
+                c = Troncons[i];
+                ctx.fillStyle = "#FFFF00";
+                ctx.fillRect(l, 140, c/10, 10);
+                ctx.fillRect(l, 165, c/10, 10);
     
+                if (Station[i] != -1) {
+                    // Dessin des stations
+                    ctx.beginPath();
+                    ctx.arc(l, 145, 10, 0, 2*Math.PI);
+                    ctx.fillStyle = '#FFFF00';
+                    ctx.fill();
+    
+                    ctx.beginPath();
+                    ctx.arc(l, 170, 10, 0, 2*Math.PI);
+                    ctx.fillStyle = '#FFFF00';
+                    ctx.fill();
+
+                    // Dessin des stations avec correspondance
+                    if(avec_corres[Station[i]]) {
+                        ctx.beginPath();
+                        ctx.arc(l, 145, 10, 0, 2 * Math.PI);
+                        ctx.fillStyle = '#000000';
+                        ctx.fill();
+                        ctx.beginPath();
+                        ctx.arc(l, 145, 7, 0, 2 * Math.PI);
+                        ctx.fillStyle = '#FFFFFF';
+                        ctx.fill();
+
+                        ctx.beginPath();
+                        ctx.arc(l, 170, 10, 0, 2 * Math.PI);
+                        ctx.fillStyle = '#000000';
+                        ctx.fill();
+                        ctx.beginPath();
+                        ctx.arc(l, 170, 7, 0, 2 * Math.PI);
+                        ctx.fillStyle = '#FFFFFF';
+                        ctx.fill();
+                    }
+                }
+
+                l+= c/10;
+            }
+        }
+
         class train {
             constructor(num_train, Matrice, Vmoy) {
                 this.position = 200;
@@ -913,25 +962,25 @@ function simulation(telecharg){
     
             dessine_aller() {
                 ctx.fillStyle = '#FF0000';
-                ctx.fillRect(this.position/10 - 10, 40, 20, 10);
+                ctx.fillRect(this.position/10 - 10, 140, 20, 10);
             }
     
             dessine_retour() {
                 ctx.fillStyle = '#FF0000';
-                ctx.fillRect((33120-this.position)/10 - 10, 65, 20, 10);
+                ctx.fillRect((33120-this.position)/10 - 10, 165, 20, 10);
             }
     
             efface_aller() {
                 ctx.fillStyle = '#FFFF00';
-                ctx.fillRect(this.position/10 - 10, 40, 20, 10);
+                ctx.fillRect(this.position/10 - 10, 140, 20, 10);
             }
     
             efface_retour() {
                 ctx.fillStyle = '#FFFF00';
-                ctx.fillRect((33120-this.position)/10 - 10, 65, 20, 10);
+                ctx.fillRect((33120-this.position)/10 - 10, 165, 20, 10);
             }
     
-            avance(temps) {
+            avance(temps, Troncons) {
                 var dt = 1;
                 if(this.aller) {
                     this.efface_aller();
@@ -940,10 +989,15 @@ function simulation(telecharg){
                     this.efface_retour();
                 }
     
+                // Passage de l'aller au retour
+                if(this.aller && this.troncon == 73 && temps == this.horaire_suivant) {
+                    this.aller = false;
+                }
+
                 // Départ après un arrêt
                 if(temps == this.horaire_suivant && !this.marche) {
                     this.marche = true;
-                    this.horaire_suivant = this.horaire_suivant + Troncons[this.troncon] / Vmoy;
+                    this.horaire_suivant = this.horaire_suivant + Math.trunc(Troncons[this.troncon] / Vmoy);
                 }
     
                 // Fin de tronçon
@@ -952,7 +1006,7 @@ function simulation(telecharg){
     
                     // Pour éviter les erreurs d'arrondis sur l'affichage graphique
                     var pos = 0;
-                    for(var i=0; i<this.troncon+1; i++)
+                    for(var i=0; i<this.troncon; i++)
                         pos+= Troncons[i]/10;
                     this.position = pos*10;
     
@@ -960,7 +1014,7 @@ function simulation(telecharg){
     
                     // Pas d'arrêt
                     if(temps_d_arret == 0) {
-                        this.horaire_suivant = this.tps_de_depart[this.troncon] + Troncons[this.troncon] / Vmoy;
+                        this.horaire_suivant = this.tps_de_depart[this.troncon] + Math.trunc(Troncons[this.troncon] / Vmoy);
                     }
                     // Arrêt
                     else {
@@ -974,11 +1028,6 @@ function simulation(telecharg){
                     this.position+= this.vitesse*dt;
                 }
     
-                // Passage de l'aller au retour
-                if(this.aller && this.position >= 16560) {
-                    this.aller = false;
-                }
-    
                 // Fin du trajet
                 if(!this.aller && this.position >= 33120) {
                     this.marche = false;
@@ -988,8 +1037,10 @@ function simulation(telecharg){
     
                 if(this.aller)
                     this.dessine_aller();
-                else this.dessine_retour();
+                else {
+                    this.dessine_retour();
                 }
+            }
         }
     
         // Création de tous les trains
@@ -1000,39 +1051,19 @@ function simulation(telecharg){
             Tableau_train.push(t);
         }
     
-        // Boucle principale : beaucoup de bazar qui fonctionne plus ou moins
+        // Boucle principale
         var temps = new Array();
         temps.push(0);
         function boucle(t) {
+            dessine_Ligne_1();
             for(var i=0; i<Nombre_de_train; i++) {
-                Tableau_train[i].avance(t[0]);
+                Tableau_train[i].avance(t[0], Troncons);
             }
             t[0] += 1;
-            ctx.fillText(t[0],10*t[0],100);
         }
-
-        function boucle2(t) {
-            for(var i=0; i<Nombre_de_train; i++) {
-                Tableau_train[i].avance(t);
-                ctx.fillText(i,10*i,125);
-            }
-            t += 1;
-            ctx.fillText(t,10*t,100);
-        }
-
-        /*
-        for(var k=0; k<tps_simula; k++) {
-            boucle2(k);
-        }
-        */
     
-        setInterval(boucle, 10, temps);
-
-        ctx.fillStyle = '#060080';
-        ctx.font = "bold 10px Arial";
-        ctx.fillText("Mince, c'est fini :(", 10, 180);
-    }
-    
+        setInterval(boucle, 5, temps);
+    }    
     
     //================================================================================================================================
     //================================================================================================================================
@@ -1059,7 +1090,5 @@ function simulation(telecharg){
     
     var c = document.getElementById("myCanvas"); 
     var ctx = c.getContext("2d"); 
-
     animation_graphique(Matrice_trains_temporel, ctx);
-    
     }
